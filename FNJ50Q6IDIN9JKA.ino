0abc19678c407b43e508e54d1d428867354f93fb
rdk707 <47817889+rdk707@users.noreply.github.com>
2019-10-07 19:04:53 +0530

    Add files via upload

---
+++ FNJ50Q6IDIN9JKA.ino
@@ -0,0 +1,27 @@
+//ERFINDER CODE
+#include <SoftwareSerial.h>
+
+SoftwareSerial mySerial(9, 10); // RX, TX
+
+void setup()
+{
+
+Serial.begin(9600);
+
+
+}
+
+void loop() // run over and over
+{
+  Serial.println("Calling through GSM Modem");
+
+// set the data rate for the SoftwareSerial port
+mySerial.begin(9600);
+delay(2000);
+mySerial.println("ATDxxxxxxxxxx;"); // ATDxxxxxxxxxx; -- watch out here for semicolon at the end!!
+
+Serial.println("Called ATDxxxxxxxxxx");
+// print response over serial port
+if (mySerial.available())
+Serial.write(mySerial.read());
+}
